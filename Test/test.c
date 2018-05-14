faa6204e3c4582ea65f1c2fd2ca4243b7a74ba70
bumfo <bumfo@users.noreply.github.com>
2018-05-14 16:24:22 +0800

    test.c

---
+++ Test/test.c
@@ -0,0 +1,83 @@
+int int const main int;
+
+a * b;
+
+return a * b;
+
+a (v);
+
+int const;
+union a;
+
+int signed * * const int a;
+
+int hello a;
+
+hello a;
+
+struct A;
+
+union B;
+
+struct {};
+
+
+struct A b;
+
+struct B {} c;
+
+extern a b;
+
+struct Hello {
+  union A {
+    int b;
+  } a;
+};
+
+typedef struct A type_hello;
+
+int a();
+
+int b() {}
+
+int c[100];
+
+int * const a, * const b, c, * d;
+
+typedef int type_a, * type_b;
+
+int a, (* const b) = &a;
+
+typedef int ((type_a)), (* type_b), (((((* volatile * const * const volatile type_c)))));
+
+int (* (* a) (int a)) ();
+
+int (* a ()) ();
+
+int (* b ()) () {}
+
+int a = 0, b = 1, c = 2;
+
+int a;
+
+int main() {
+
+}
+
+unsigned long long
+fuck ()
+{
+  int a;
+}
+
+unsigned long long
+fuck (),
+shit ();
+
+int
+( * b ( )
+)
+(
+)
+{
+}
