241fafd326bbff3e648dbf8df3278e3b13b3f95f
bumfo <bumfo@users.noreply.github.com>
2018-05-15 12:54:06 +0800

    test.c

--- Test/test.c
+++ Test/test.c
@@ -9,6 +9,8 @@ a (v);
 int const;
 union a;
 
+int * a;
+
 int signed * * const int a;
 
 int hello a;
@@ -81,3 +83,154 @@ int
 )
 {
 }
+
+hello world()
+{
+}
+
+123;
+
+(a * (b * c + 1));
+
+A a() {
+  0[123];
+
+  abc (123, 456);
+  abc(123, 456);
+
+  (abc, abc)(abc, abc)[1];
+  (abc, abc) (abc, abc) [1];
+  [1][1];
+  [1] [1];
+
+  {}
+
+  return {};
+}
+
+0++()+++ ++1;
+
+1 * 2 + 3 / 4 - 6;
+
++(+1+ +2 * * 2);
+
+a * b;
+
+1 + a * b;
+
+
+for (int i = 0) {
+}
+
+for (i = 0) {
+}
+
+
+for (int i = 0; i < 10) {
+}
+
+for (i = 0; i < 10) {
+}
+
+
+for (int i = 0; i < 10; ++i) {
+}
+
+for (i = 0; i < 10; ++i) {
+}
+
+abc(123 + 456, abc * d, e);
+
+1 + 2 + 3 + 4;
+
+a ? 0 : 1 : 1;
+
+a, b, c;
+
+1 + for + c;
+
+a[1][2][3][4];
+
+!!!a;
+
+1 + FALSE + 2;
+1 + FALSE() + 2;
+
+int main(int a, char * args[]);
+
+int a(a, b, c) int a, b, c; {
+}
+
+void f(void f(int), void (* g)()) {
+}
+
+void (* g(int i)) (int);
+
+void (* g(int i)) (int) {
+}
+
+// // abc
+
+/* /*  */
+
+{
+  for (int i = 0; i < n; ++i) {
+    printf("%d\n", i + 123);
+  }
+}
+
+#include <stdio.h>
+#include "test.h"
+
+#   include <stdio.h>
+#   include "test.h"
+
+
+#\
+/*he\
+llo*/\
+include\
+\
+/*he\
+llo*/\
+<std\
+io.h> // hello
+
+
+1, 01, 08, 0, 0xfff,
+
+1;
+1U;
+1L;
+1LL;
+1UL;
+1ULL;
+1LU;
+1LLU;
+
+1UU;
+1LLL;
+
+0;
+
+00;
+
+"123\"";
+123;
+
+'\0';
+
+'\008';
+'\00000';
+
+return (int) 0;
+
+"hello/*123*/hello"
+
+"abc\
+123";
+
+"abc
+def"ghi;
+
+"abc\def";
