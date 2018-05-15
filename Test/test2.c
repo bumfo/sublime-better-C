d1c9d8171ff2d8249cd805c53a3ce003607e95ec
bumfo <bumfo@users.noreply.github.com>
2018-05-15 13:15:49 +0800

    test2.c

---
+++ Test/test2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int f(int i) {
+  if (i == 0) {
+    return 1;
+  } else {
+    int j = 0;
+
+    switch (i) {
+      case 1:
+        return f(3);
+      case 2:
+        break;
+      default:
+        goto done;
+    }
+
+    for (int k = 0; k < i; ++k) {
+      if (i == 0) {
+        continue;
+      }
+
+      do {
+        while (true) {
+          ++j;
+          goto done;
+        }
+      } while (true);
+    }
+
+
+done:
+    return f(j);
+  }
+}
+
+int main() {
+  printf("%d\n", f(2));
+}
